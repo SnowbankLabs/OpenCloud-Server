f52d1503de9f0614e101b71536e3e4100cb6c994
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-02-23 22:40:47 -0800

    Created initial routes for auth system

--- src/systems/auth/auth.routes.ts
+++ src/systems/auth/auth.routes.ts
@@ -1,8 +1,27 @@
 import type { FastifyInstance } from "fastify";
 
+import { $ref } from "./auth.schemas";
+import { createUserHandler, loginHandler } from "./auth.handlers";
+
 async function authRouter(server: FastifyInstance) {
-    server.get("/", async () => {
-        return { hello: "world" };
+    server.route({
+        method: "POST",
+        url: "/create",
+        schema: {
+            body: $ref("createUserSchema"),
+            response: $ref("createUserResponseSchema"),
+        },
+        handler: createUserHandler,
+    });
+
+    server.route({
+        method: "POST",
+        url: "/login",
+        schema: {
+            body: $ref("loginSchema"),
+            response: $ref("loginResponseSchema"),
+        },
+        handler: loginHandler,
     });
 }
 
