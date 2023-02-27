c47c343505ca5d4449acb1a433ba9395da594e41
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-02-27 10:46:08 -0800

    Added eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+    overrides: [
+        {
+            extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended-requiring-type-checking"],
+            files: ["*.ts"],
+            parserOptions: {
+                project: "tsconfig.json",
+            },
+        },
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json",
+    },
+    plugins: ["@typescript-eslint"],
+    extends: ["plugin:@typescript-eslint/recommended"],
+    rules: {
+        "@typescript-eslint/consistent-type-imports": "warn",
+    },
+};
