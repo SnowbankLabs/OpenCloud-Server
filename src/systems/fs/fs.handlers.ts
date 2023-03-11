a728f16ba30f0b9903bf6fb8c4cc76f04482cb17
Michael Wang <41971562+devmwang@users.noreply.github.com>
2023-03-10 20:24:27 -0800

    Updated content-disposition header for file get req

--- src/systems/fs/fs.handlers.ts
+++ src/systems/fs/fs.handlers.ts
@@ -158,7 +158,7 @@ export async function getFileHandler(
     }
 
     void reply.header("Content-Type", fileData.fileType);
-    void reply.header("Content-Disposition", `inline; filename="${fileData.fileName}"`);
+    void reply.header("Content-Disposition", `filename="${fileData.fileName}"`);
 
     return reply.sendFile(fileData.ownerId + "/" + fileData.id);
 }
